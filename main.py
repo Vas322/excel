567cd139d0394e9100c49047f74f6d6c71728a93
vasily <artur.dvo@mail.ru>
2021-11-07 12:51:18 +0300

    first commit - create 2 lists for projects

---
+++ main.py
@@ -0,0 +1,23 @@
+import openpyxl
+
+
+def win_rate():
+    wb = openpyxl.reader.excel.load_workbook(filename='test.xlsx', data_only=True)
+    projects = []
+    projects_win = []
+    sheet = wb.worksheets[0]  # Обращение к определенному листу. 0 - это 1-й лист
+    for row in range(2, sheet.max_row + 1):
+        # max_row - проходит до конца значений по столбцу
+        id_project = sheet[row][0].value  # 0,1,2...n - идентификаторы столбцов слева направо
+        if id_project is not None:
+            projects.append(id_project)
+        id_project_win = sheet[row][1].value
+        if id_project_win is not None:
+            projects_win.append(id_project_win)
+    print(f'Проекты: {projects}')
+    print(f'Отгруженные проекты: {projects_win}')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    win_rate()
