0dd4ebd5427b3168aa98698a82fed2ec3f0df220
vasily <artur.dvo@mail.ru>
2021-11-08 23:24:09 +0300

    Check the count of sheets. Check that a number has been entered.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import openpyxl
 
+wb = openpyxl.reader.excel.load_workbook(filename='test.xlsx', data_only=True)
+count_of_lists = len(wb.sheetnames)  # количество листов в книге
+
 
 def get_count_of_elements(list):
     count = 0
@@ -8,11 +11,19 @@ def get_count_of_elements(list):
     return count
 
 
+def input_lists(msg):
+    input_data = input(msg)
+    if not input_data.isdigit(): return input_lists("Вы ввели не число. Введите целое положительное число: ")
+    if 0 <= int(input_data) <= count_of_lists: return int(input_data) - 1
+    return input_lists(f'Всего листов в книге: {count_of_lists}.'
+                       f' Введите число от 1 до {count_of_lists}. ')
+
+
 def win_rate():
-    wb = openpyxl.reader.excel.load_workbook(filename='test1.xlsx', data_only=True)
     projects = []
     projects_win = []
-    sheet = wb.worksheets[0]  # Обращение к определенному листу. 0 - это 1-й лист
+    list_excel = input_lists("Введите номер листа книги Ecxel: ")
+    sheet = wb.worksheets[list_excel]  # Обращение к определенному листу. 0 - это 1-й лист
     for row in range(3, sheet.max_row + 1):  # max_row - проходит до конца значений по столбцу
         id_project = sheet[row][0].value  # 0,1,2...n - идентификаторы столбцов слева направо
         if id_project is not None:
@@ -28,7 +39,7 @@ def win_rate():
         projects_win_rate = round(count_projects_win / count_projects * 100, 1)  # round - округление до одного знака
     else:
         projects_win_rate = 0
-        print(f'Количество зарегистрированных проектов равно {projects_win_rate}. Проверьте файл!')
+        return print(f'Количество зарегистрированных проектов равно {projects_win_rate}. Проверьте файл!')
     print(f'Проекты: {projects},\nЗарегистрированных проектов: {count_projects}')
     print(f'Отгруженные проекты: {projects_win},\nВсего выигранных проектов: {count_projects_win}')
     print(f'WinRate равен: {projects_win_rate} %')
